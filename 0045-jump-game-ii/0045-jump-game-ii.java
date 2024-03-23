23894dec4ed31bd5f0278af301963c70b91e30d7
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-03-24 01:14:14 +0530

    Time: 2 ms (76.88%), Space: 44.9 MB (75.33%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        // TC: O(N) SC: O(1)
+        int res = 0;
+        int l = 0, r = 0;
+        
+        while(r < nums.length-1){
+            int farthest = 0;
+            for(int i = l; i < r+1; i++){
+                farthest = Math.max(farthest, i+nums[i]);
+            }
+            l = r+1;
+            r = farthest;
+            res++;
+        }
+        return res;
+    }
+}
