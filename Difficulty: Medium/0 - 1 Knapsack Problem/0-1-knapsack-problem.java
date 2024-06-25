0965dd137b6910afb2ff1fdb09d06cf3c5c2efb4
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-06-25 12:38:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int[] prev = new int[W+1];
+        for(int i = 0 ; i < W+1 ; i++) prev[i] = 0;
+         
+        // if(wt[0] <= W) prev[wt[0]] = val[0];
+        for(int i=wt[0];i<=W;i++){
+             prev[i]=val[0];
+         }
+        
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int cap = W ; cap >= 0 ; cap--){
+                int notTake = prev[cap];
+                int take = Integer.MIN_VALUE;
+                if(wt[ind] <= cap) take = val[ind] + prev[cap - wt[ind]];
+                prev[cap] = Math.max(take, notTake);
+            }
+        }
+        return prev[W];
+    } 
+}
+
+
