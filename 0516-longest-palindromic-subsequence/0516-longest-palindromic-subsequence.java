3c9a1345ada541408a5097db7d58f3e9d9b33934
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-07-08 10:32:07 +0530

    Time: 54 ms (44.43%), Space: 45.1 MB (86.5%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,21 +1,42 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) {
+    public int longestPalindromeSubseq(String s) { 
+        // O(n*n) O(2n)
         int n = s.length();
         String t = new StringBuilder(s).reverse().toString();
 
-        int[][] dp = new int[n+1][n+1];
+        int[] prev = new int[n+1];
 
-        for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
-        for(int j = 0 ; j <= n ; j++) dp[0][j] = 0;
+        for(int j = 0 ; j <= n ; j++) prev[j] = 0;
 
         for(int i = 1 ; i <= n ; i++){
+            int[] curr = new int[n+1];
             for(int j = 1 ; j <= n ; j++){
-                if(s.charAt(i-1) == t.charAt(j-1)) 
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    curr[j] = 1 + prev[j-1];
+                else curr[j] = Math.max(prev[j], curr[j-1]);
             }
+            prev = curr;
         }
 
-        return dp[n][n];
+        return prev[n];
+
+        // // O(n*n) o(n*n)
+        // int n = s.length();
+        // String t = new StringBuilder(s).reverse().toString();
+
+        // int[][] dp = new int[n+1][n+1];
+
+        // for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
+        // for(int j = 0 ; j <= n ; j++) dp[0][j] = 0;
+
+        // for(int i = 1 ; i <= n ; i++){
+        //     for(int j = 1 ; j <= n ; j++){
+        //         if(s.charAt(i-1) == t.charAt(j-1)) 
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        //     }
+        // }
+
+        // return dp[n][n];
     }
 }
