fc5b9746b38766f0b203c953d6023347fad914f2
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-07-08 09:30:24 +0530

    Time: 29 ms (24.12%), Space: 47 MB (85.25%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -2,28 +2,28 @@ class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int n = str1.length(), m = str2.length();
         int[][] dp = new int[n+1][m+1];
-        
-        for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;
+
         for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
-        
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(str1.charAt(i-1) == str2.charAt(j-1)) 
+        for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1 ; j<=m ; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1))
                     dp[i][j] = 1 + dp[i-1][j-1];
                 else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        
+
         String ans = "";
         int i = n, j = m;
-        
+
         while(i > 0 && j > 0){
             if(str1.charAt(i-1) == str2.charAt(j-1)){
                 ans += str1.charAt(i-1);
                 i--;
                 j--;
             }
-            else if (dp[i-1][j] > dp[i][j-1]){
+            else if(dp[i-1][j] > dp[i][j-1]){
                 ans += str1.charAt(i-1);
                 i--;
             }
@@ -32,7 +32,7 @@ class Solution {
                 j--;
             }
         }
-        
+
         while(i > 0){
             ans += str1.charAt(i-1);
             i--;
@@ -42,8 +42,8 @@ class Solution {
             ans += str2.charAt(j-1);
             j--;
         }
-        
-        String ans2 = new StringBuilder(ans).reverse().toString();
-        return ans2;
+
+        String res = new StringBuilder(ans).reverse().toString();
+        return res;
     }
 }
