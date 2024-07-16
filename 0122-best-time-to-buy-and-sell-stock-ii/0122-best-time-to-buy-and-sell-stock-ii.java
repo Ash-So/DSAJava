dd709a6c77426a0867e8ce7c73b796b48f29af62
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-07-16 11:27:50 +0530

    Time: 1 ms (89.9%), Space: 45.7 MB (34.58%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0], profit = 0;
-        for(int i = 1; i < prices.length ;i++){
-            if(prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
+        // int min = prices[0], profit = 0;
+        // for(int i = 1; i < prices.length ;i++){
+        //     if(prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
+        // }
+        // return profit;
+
+        // Tabulation
+        // O(N*2) O(N*2)
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    profit = Math.max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
+                }
+                else{
+                    profit = Math.max(prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
         }
-        return profit;
+
+        return dp[0][1];
     }
 }
