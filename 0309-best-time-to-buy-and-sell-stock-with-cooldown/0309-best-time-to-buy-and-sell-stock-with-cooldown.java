6ccc704d7c5b6098e45e9ae98016db5d3f25fefd
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-07-18 12:35:36 +0530

    Time: 1 ms (82.69%), Space: 41.5 MB (58.88%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,23 +1,41 @@
 class Solution {
     public int maxProfit(int[] prices) {
         // Similar to Best Time to Buy and Sell Stock II
+        // Space Optimization
+        // O() O()
+        // int n = prices.length;
+        // int[] front2 = new int[2];
+        // int[] front1 = new int[2];
+        // int[] curr = new int[2];
+
+    
         // Tabulation
-        // O(2*N) O(2*N)
+        // O(N) O(2*N)
         int n = prices.length;
         int[][] dp = new int[n+2][2];
         for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                int profit = 0;
-                if(buy == 1){
-                    profit = Math.max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
-                }
-                else{
-                    profit = Math.max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0]);
-                }
-                dp[ind][buy] = profit;
-            }
+            dp[ind][1] = Math.max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
+
+            dp[ind][0] = Math.max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0]);
         }
         return dp[0][1];
+
+        // O(2*N) O(2*N)
+        // int n = prices.length;
+        // int[][] dp = new int[n+2][2];
+        // for(int ind = n-1 ; ind >= 0 ; ind--){
+        //     for(int buy = 0 ; buy <= 1 ; buy++){
+        //         int profit = 0;
+        //         if(buy == 1){
+        //             profit = Math.max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);
+        //         }
+        //         else{
+        //             profit = Math.max(prices[ind] + dp[ind+2][1], 0 + dp[ind+1][0]);
+        //         }
+        //         dp[ind][buy] = profit;
+        //     }
+        // }
+        // return dp[0][1];
         
     }
 }
