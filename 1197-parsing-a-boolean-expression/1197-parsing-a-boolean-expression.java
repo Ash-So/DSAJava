393c5ae2bdade8c7d8414cbed777828c49ab96f8
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-07-25 11:49:10 +0530

    Time: 13 ms (37.36%), Space: 44.4 MB (37.59%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean parseBoolExpr(String expression) {
+        int n = expression.length();
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0 ; i < n ; i++){
+            char ch = expression.charAt(i);
+
+            if(ch == ')'){
+                HashSet<Character> hp = new HashSet<>();
+                while(st.peek() != '('){
+                    hp.add(st.pop());
+                }
+                st.pop();
+                char op = st.pop();
+                if(op == '!'){
+                    st.push(hp.contains('t') ? 'f' : 't');
+                }
+                else if(op == '&'){
+                    st.push(hp.contains('f') ? 'f' : 't');
+                }
+                else{
+                    st.push(hp.contains('t') ? 't' : 'f');
+                }
+            }
+            else if(ch != ','){
+                st.push(ch);
+            }
+        }
+        return st.peek() == 't' ? true : false;
+    }
+}
