b363ac0cc68f9ae1125ccac6b5570a2d7cfb46cb
Ash-So <142682875+Ash-So@users.noreply.github.com>
2024-08-11 13:32:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -31,46 +31,78 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair {
-    int first;
-    int second;
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second; 
-    }
-}
-class Solution {
-    public boolean detect(int src, ArrayList<ArrayList<Integer>> adj, int[] vis){
-        vis[src] = 1;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(src, -1));
+// // BFS
+// class Pair {
+//     int first;
+//     int second;
+//     public Pair(int first, int second) {
+//         this.first = first;
+//         this.second = second; 
+//     }
+// }
+// class Solution {
+//     public boolean detect(int src, ArrayList<ArrayList<Integer>> adj, int[] vis){
+//         vis[src] = 1;
+//         Queue<Pair> q = new LinkedList<>();
+//         q.add(new Pair(src, -1));
         
-        while(!q.isEmpty()){
-            int node = q.peek().first;
-            int parent = q.peek().second;
-            q.remove();
+//         while(!q.isEmpty()){
+//             int node = q.peek().first;
+//             int parent = q.peek().second;
+//             q.remove();
             
-            for(int adjNode : adj.get(node)){
-                if(vis[adjNode] == 0){
-                    vis[adjNode] = 1;
-                    q.add(new Pair(adjNode, node));
-                }
-                else if(parent != adjNode)
+//             for(int adjNode : adj.get(node)){
+//                 if(vis[adjNode] == 0){
+//                     vis[adjNode] = 1;
+//                     q.add(new Pair(adjNode, node));
+//                 }
+//                 else if(parent != adjNode)
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+//     // Function to detect cycle in an undirected graph.
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//         // O(N + 2E) + O(N)  O(N) + O(N)
+//         int[] vis = new int[V];
+//         for(int i = 1 ; i <= V ; i++){
+//             if(vis[i-1] == 0){
+//                 if(detect(i-1, adj, vis) == true) return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// DFS
+class Solution {
+    public boolean dfs(int node, int parent, int[] vis, ArrayList<ArrayList<Integer>> adj){
+        vis[node] = 1;
+        
+        for(int adjNode : adj.get(node)){
+            if(vis[adjNode] == 0){
+                if(dfs(adjNode, node, vis, adj) == true){
                     return true;
+                }
+            }
+            else if(parent != adjNode){
+                return true;
             }
         }
         return false;
     }
+    
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        // O(N + 2E) + O(N)  O(N) + O(N)
+        // O(N + 2E + N ) O(N + N)
         int[] vis = new int[V];
-        for(int i = 1 ; i <= V ; i++){
-            if(vis[i-1] == 0){
-                if(detect(i-1, adj, vis) == true) return true;
+        
+        for(int i = 0 ; i < V ; i++){
+            if(vis[i] == 0){
+                if(dfs(i, -1, vis, adj) == true) return true;
             }
         }
-        
         return false;
     }
 }
